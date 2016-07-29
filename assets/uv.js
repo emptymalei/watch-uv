440395c531b6cf01b16c81f78d7157714e5251e6
emptymalei <emptymalei@qq.com>
2016-07-29 15:22:14 -0600

    :boom: working example

--- assets/uv.js
+++ assets/uv.js
@@ -1,11 +1,50 @@
+locpostal = 87106;
+
+function indexFun(locpostal) {
+
+urlJSONOriginal = 'https://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/'+locpostal+'/JSONP';
+
+urlJSONProxy = 'https://jsonp.afeld.me/?callback=&url=https%3A%2F%2Fiaspub.epa.gov%2Fenviro%2Fefservice%2FgetEnvirofactsUVHOURLY%2FZIP%2F'+locpostal+'%2FJSON';
+
+var html='';
+var barxlabel = [];
+var baryvalue = [];
+
+
+
 $(document).ready(function() {
     $.ajax({
-        url: "https://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/87106/JSON",
+        url: urlJSONProxy,
         type: "GET",
-        rossDomain: true,
-        dataType: "json"
-    }).then(function(data) {
-       $('.order').append(data.ORDER);
-       $('.uv-value').append(data.UV_VALUE);
+        crossDomain: true,
+        dataType: "json",
+        success: function(data) {
+        for(entry=0; entry < data.length;entry++) {
+            var order = data[entry]['ORDER'];
+            var timestp = data[entry]['DATE_TIME'];
+            var value = data[entry]['UV_VALUE'];
+            var zipvalue = data[entry]['ZIP'];
+
+              barxlabel.push(timestp.substr(timestp.length - 5))
+              baryvalue.push(value)
+             html+='<tr>'+'<td>'+order+'</td>'+'<td>'+timestp+'</td>'+'<td>'+value+'</td>'+'<td>'+zipvalue+'</td>'+'</tr>'
+            }
+            $("#table-data").append(html);
+
+            var barplotdata = [{
+              x: barxlabel.slice(2,15),
+              y: baryvalue.slice(2,15),
+              type: 'bar'
+            }];
+            var layout = {
+	               title: 'Hourly UV Index ('+locpostal+')',
+	                  showlegend: false};
+
+                Plotly.newPlot('pltlybar', barplotdata,layout,{showLink: false});
+
+        }
+
     });
 });
+
+} // END OF GRAND FUNCTION
